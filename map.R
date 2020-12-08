a81ccfb6cab0eb5fee0946c297f3f64b0d0554ab
Kyle Bocinsky <bocinsky@gmail.com>
2020-12-08 09:59:53 -0700

    viewers

--- map.R
+++ map.R
@@ -66,4 +66,38 @@ plotly::ggplotly(
 
 webinar_donations <- 
   readxl::read_excel("Webinar Donations - December 2020.xlsx") %>%
-  dplyr::select(Date = `Event Date`)
+  dplyr::select(Date = `Event Date`,
+                Donations = `Donation Amount`) %>%
+  dplyr::mutate(Date = lubridate::as_date(Date)) %>%
+  dplyr::group_by(Date) %>%
+  dplyr::summarise(Donations = sum(Donations, na.rm = TRUE)) %>%
+  dplyr::left_join(webinar_metrics %>%
+                     dplyr::select(Date, Name) %>%
+                     dplyr::distinct(),
+                   .) %>%
+  dplyr::arrange(Date)
+
+plotly::ggplotly(
+  ggplot(webinar_donations) +
+    geom_line(aes(x = Date,
+                  y = Donations,
+                  text = paste0(Name, "\n",
+                                Date, "\n",
+                                "Amount: ", Donations),
+                  group = 1)) +
+    scale_x_date(date_breaks = "1 month",
+                 date_labels = "%b") +
+    xlab("Date") +
+    ylab("Donations") +
+    theme_minimal() +
+    theme(legend.justification = c(1,1),
+          legend.position = c(1,1),
+          legend.background=element_blank(),
+          plot.margin = margin(0.5,0.5,0.5,0.5, unit = "cm"),
+          panel.grid.major.x = element_blank(),
+          axis.text = element_text(size = 12),
+          axis.title = element_text(size = 16)
+    ),
+  tooltip = "text"
+) %>%
+  htmlwidgets::saveWidget("donations.html")
